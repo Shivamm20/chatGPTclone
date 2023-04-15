c078c7e10192c56f2404270ade656d07f54d6cbd
shivamtiwari <om8209053@gmail.com>
2023-04-15 22:15:52 +0530

    first commit

---
+++ controllers/authController.js
@@ -0,0 +1,69 @@
+const errorHandler = require("../middlewares/errorMiddleware");
+const userModel = require("../models/userModel");
+const errorResponse = require("../utils/errorResponse");
+
+// JWT TOKEN
+exports.sendToken = (user, statusCode, res) => {
+    try {
+        const token = user.getSignedToken(res);
+        console.log(token);
+        res.status(statusCode).json({
+            success: true,
+            token: "hello",
+        });
+    } catch (error) {
+        res.status(500).send(error);
+    }
+};
+
+//REGISTER
+exports.registerContoller = async (req, res, next) => {
+    try {
+        const { username, email, password } = req.body;
+        //exisitng user
+        const exisitingEmail = await userModel.findOne({ email });
+        console.log(exisitingEmail);
+        if (exisitingEmail) {
+            return next(new errorResponse("Email is already register", 500));
+        }
+        const user = await userModel({ username, email, password });
+        this.sendToken(user, 201, res);
+        // res.status(200).send({ msg: "Registration Successful!" })
+    } catch (error) {
+        console.log(error);
+        next(error);
+    }
+};
+
+//LOGIN
+exports.loginController = async (req, res, next) => {
+    try {
+        const { email, password } = req.body;
+        //validation
+        if (!email || !password) {
+            return next(new errorResponse("Please provide email or password"));
+        }
+        const user = await userModel.findOne({ email });
+        if (!user) {
+            return next(new errorResponse("Invalid Creditial", 401));
+        }
+        const isMatch = await user.matchPassword(password);
+        if (!isMatch) {
+            return next(new errorResponse("Invalid Creditial", 401));
+        }
+        //res
+        this.sendToken(user, 200, res);
+    } catch (error) {
+        console.log(error);
+        next(error);
+    }
+};
+
+//LOGOUT
+exports.logoutController = async (req, res) => {
+    res.clearCookie("refreshToken");
+    return res.status(200).json({
+        success: true,
+        message: "Logout Succesfully",
+    });
+};
---
+++ server.js
@@ -0,0 +1,37 @@
+const express = require('express');
+const morgan = require("morgan");
+const cors = require('cors');
+const bodyParser = require('body-parser');
+const colors = require('colors');
+const dotenv = require('dotenv');
+const connectDB = require('./config/db');
+const errorHandler = require('./middlewares/errorMiddleware');
+const cookie = require('cookie');
+
+// routes path
+
+const authRoutes = require('./routes/authRoutes');
+
+
+
+dotenv.config();
+connectDB();
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+app.use(bodyParser.urlencoded({ extended: false }))
+app.use(morgan('dev'));
+app.use(errorHandler);
+
+const PORT = process.env.PORT || 8080;
+
+// API routes
+
+app.use('/api/v1/auth', authRoutes);
+app.use('/api/v1/openai', require("./routes/openaiRoutes"));
+
+app.listen(PORT, () => {
+    console.log(`server running in ${process.env.DEV_Mode} mode on port ${PORT}`.bgCyan.white);
+})
